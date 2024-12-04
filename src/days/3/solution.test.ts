dbb972f1f1e401c57b9f945db174db7d7e4175b1
SlatinskyJ <jakub.slatinsky@gmail.com>
2024-12-04 20:05:11 +0100

    feat: solve part2 of day 3

--- src/days/3/solution.test.ts
+++ src/days/3/solution.test.ts
@@ -12,8 +12,8 @@ describe('On Day 3', () => {
 
 	test(`example part2`, () => {
 		expect(
-			day3.solveForPartOne(
-				'xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))\n'
+			day3.solveForPartTwo(
+				"xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
 			)
 		).toBe('48');
 	});
--- src/days/3/solution.ts
+++ src/days/3/solution.ts
@@ -1,5 +1,10 @@
 import { Day } from 'templates/day';
 
+type IMulInstruction = [number, number];
+type IFlagInstruction = boolean;
+
+type IInstruction = IMulInstruction | IFlagInstruction;
+
 class Day3 extends Day {
 	constructor() {
 		super(3);
@@ -8,28 +13,72 @@ class Day3 extends Day {
 	solveForPartOne(input: string): string {
 		let sum = 0;
 
-		const instructions = this.findInstructions(input);
-		sum += this.executeAndAdd(instructions);
+		const instructions = this.findInstructions(
+			input,
+			/mul\(\d{1,3},\d{1,3}\)/gm
+		);
+		sum += this.executeInstructions(instructions);
 
 		return `${sum}`;
 	}
 
-	findInstructions(input: string): [number, number][] {
-		const instructions = input.match(/mul\(\d{1,3},\d{1,3}\)/gm);
+	findInstructions(input: string, regexp: RegExp): IInstruction[] {
+		const instructions = input.match(regexp);
 		if (instructions === null) {
 			return [];
 		}
+
 		return instructions
-			.map((instruction) => instruction.match(/\d+/gm))
-			.map((pair) => pair!.map(Number)) as [number, number][];
+			.map((instruction) => instruction.match(/\d+|do\(\)|don't\(\)/gm))
+			.map((ins) => {
+				if (ins?.toString() === 'do()') {
+					return true;
+				}
+				if (ins?.toString() === "don't()") {
+					return false;
+				}
+
+				return ins!.map(Number);
+			}) as [number, number][];
 	}
 
-	executeAndAdd(instructions: [number, number][]): number {
-		return instructions.reduce((acc, [a, b]) => acc + a * b, 0);
+	executeInstructions(instructions: IInstruction[]): number {
+		let currentFlag = true;
+
+		return instructions.reduce((acc, instruction) => {
+			const result = this.executeInstruction(instruction);
+			if (typeof result === 'boolean') {
+				currentFlag = result;
+				return acc;
+			}
+			if (currentFlag) {
+				return acc + result;
+			}
+			return acc;
+		}, 0);
+	}
+
+	executeInstruction(instruction: IInstruction): number | boolean {
+		if (typeof instruction === 'boolean') {
+			return instruction;
+		}
+		if (typeof instruction === 'object') {
+			return instruction[0] * instruction[1];
+		}
+
+		return 0;
 	}
 
 	solveForPartTwo(input: string): string {
-		return input;
+		let sum = 0;
+
+		const instructions = this.findInstructions(
+			input,
+			/mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)/gm
+		);
+		sum += this.executeInstructions(instructions);
+
+		return `${sum}`;
 	}
 }
 
