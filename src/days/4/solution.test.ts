d88af95e93fc71cd34ac9ac1457ddebf7471e3c3
SlatinskyJ <jakub.slatinsky@gmail.com>
2024-12-06 09:01:26 +0100

    feat: solve part2 of day 4

--- src/days/4/solution.test.ts
+++ src/days/4/solution.test.ts
@@ -64,6 +64,18 @@ const overEdge =
 	'........\n' +
 	'........';
 
+const exampleInput2 =
+	'.M.S......\n' +
+	'..A..MSMS.\n' +
+	'.M.S.MAA..\n' +
+	'..A.ASMSM.\n' +
+	'.M.S.M....\n' +
+	'..........\n' +
+	'S.S.S.S.S.\n' +
+	'.A.A.A.A..\n' +
+	'M.M.M.M.M.\n' +
+	'..........';
+
 describe('On Day 4', () => {
 	test(`part1 solve example`, () => {
 		expect(day4.solveForPartOne(exampleInput)).toBe('18');
@@ -94,5 +106,7 @@ describe('On Day 4', () => {
 		expect(day4.solveForPartOne('...S\n..A.\n.M..\nX...')).toBe('1');
 	});
 
-	test(`part2 example`, () => {});
+	test(`part2 example`, () => {
+		expect(day4.solveForPartTwo(exampleInput2)).toBe('9');
+	});
 });
--- src/days/4/solution.ts
+++ src/days/4/solution.ts
@@ -28,8 +28,33 @@ class Day4 extends Day {
 		return `${test}`;
 	}
 
+	createXReg(regexp: string, parameter: number) {
+		//'/A(((?<=M.{10})(?=.{9}S))|((?<=S.{10})(?=.{9}M)))((?<=S.{8})(?=.{7}M)|(?<=M.{8})(?=.{7}S))/gs'
+		// A = 10
+		// B = 9
+		// C = 8
+		// D = 7
+		const matcher = (
+			[
+				['{valA}', parameter + 2],
+				['{valB}', parameter + 1],
+				['{valC}', parameter],
+				['{valD}', parameter - 1],
+			] satisfies [string, number][]
+		).reduce((acc, [i, val]) => acc.replaceAll(i, `${val}`), regexp);
+
+		return new RegExp(matcher, 'gs');
+	}
+
 	solveForPartTwo(input: string): string {
-		return input;
+		const length = input.split('\n').length;
+
+		const matcher =
+			'A(((?<=M.{{valA}})(?=.{{valB}}S))|((?<=S.{{valA}})(?=.{{valB}}M)))((?<=S.{{valC}})(?=.{{valD}}M)|(?<=M.{{valC}})(?=.{{valD}}S))';
+
+		const myRegEx = this.createXReg(matcher, length);
+
+		return `${input.match(myRegEx)!.length}`;
 	}
 }
 
